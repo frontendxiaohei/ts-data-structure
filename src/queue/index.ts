e4ad1d393f5460830a3dd90d8f3517e0b3d5aa19
big whiter <38613864+frontendxiaohei@users.noreply.github.com>
2024-11-12 16:12:50 +0800

    feat: 栈和队列自定义实现

--- src/queue/index.ts
+++ src/queue/index.ts
@@ -1,3 +1,4 @@
+// 直接调用数组已有的队列方法
 class Queue<T> {
     private items: T[] = [];
     enqueue(item: T): void {
@@ -12,6 +13,14 @@ class Queue<T> {
     isEmpty(): boolean {
         return this.items.length === 0;
     }
+
+    tail() {
+        return this.items[this.items.length - 1];
+    }
+
+    size() {
+        return this.items.length;
+    }
 }
 
 // test
@@ -24,3 +33,70 @@ queue.enqueue(5);
 console.log(queue.dequeue());
 console.log(queue.front());
 console.log(queue.isEmpty());
+
+console.log('----------------------------------------------');
+
+
+// 自己实现
+class Queue2<T> {
+    queue: T[] = [];
+    l: number
+    r: number
+
+    constructor(n: number) {
+        this.queue = new Array(n);
+        this.l = 0;
+        this.r = 0;
+    }
+
+    isEmpty() : boolean {
+        return this.l == this.r
+    }
+
+    enqueue(num: T) {
+        if(this.r == this.queue.length) {
+            throw new Error("队列已满")
+        }
+        this.queue[this.r++] = num
+    }
+
+    dequeue() {
+        if(this.l == this.r) {
+            throw new Error("队列已空")
+        }
+        return this.queue[this.l++]
+    }
+
+    front() {
+        if(this.l == this.r) {
+            throw new Error("队列已空")
+        }
+        return this.queue[this.l]
+    }
+
+    tail() {
+        if(this.l == this.r) {
+            throw new Error("队列已空")
+        }
+        return this.queue[this.r - 1]
+    }
+
+    size() {
+        return this.r - this.l
+    }
+
+}
+
+// test
+let queue2 = new Queue2<number>(4);
+queue2.enqueue(200);
+queue2.enqueue(2);
+queue2.enqueue(3);
+queue2.enqueue(4);
+// queue2.enqueue(5);
+console.log(queue2.dequeue());
+console.log(queue2.front());
+console.log(queue2.isEmpty());
+console.log(queue2.tail());
+
+
--- src/stack/index.ts
+++ src/stack/index.ts
@@ -26,6 +26,52 @@ console.log(stack.pop())
 console.log(stack.peek());
 console.log(stack.isEmpty());
 
+console.log('------------------------------');
+
+
+class Stack2<T> {
+    stack: T[] = [];
+    size: number
+
+    constructor(n: number) {
+        this.stack = new Array(n);
+        this.size = 0
+    }
+
+    isEmpty() : boolean {
+        return this.size == 0
+
+    }
+
+    push(num: T) {
+        if(this.size == this.stack.length) {
+            throw new Error("栈已满")
+        }
+        this.stack[this.size++] = num
+    }
+
+    pop() {
+        return this.stack[--this.size]
+    }
+
+    peek() {
+        return this.stack[this.size - 1]
+    }
+}
+
+// test
+let stack2 = new Stack2<number>(5);
+stack2.push(1);
+stack2.push(2);
+stack2.push(3);
+stack2.push(4);
+stack2.push(5);
+console.log(stack2.pop())
+console.log(stack2.peek());
+console.log(stack2.isEmpty());
+
+
+
 
 
 
